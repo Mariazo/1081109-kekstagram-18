8b3444899adeb070d44ef2ba3af19ace070d55a5
Mariazo <zmbereza@gmail.com>
2019-10-01 16:23:25 +0300

    remarks

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@ var getPictureList = function (pictureCount) {
       var likesCount = randomNumber(LIKES_COUNT_MAX, LIKES_COUNT_MIN);
       var pictureComments = getCommentsList(randomNumber(2, 1));
       tempArray.push(getRandomPictureItem(pictureUrl, pictureDiscription, likesCount, pictureComments));
-    )};
+    });
   return tempArray;
 };
 
