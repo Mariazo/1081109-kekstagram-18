67415068c82f724b5f1633d4e2ad64c684fe3bfc
Mariazo <zmbereza@gmail.com>
2019-10-13 20:41:03 +0300

    8 ht

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var MIN_LIKES_COUNT = 15;
 var MAX_LIKES_COUNT = 200;
 var MIN_AVATAR_NUMBER = 1;
 var MAX_AVATAR_NUMBER = 6;
+var TAG_NUMBER = 5;
+var ESC_BUTTON = 27;
 
 var AUTHOR_COMMENTS = [
   'Всё отлично!',
@@ -96,3 +98,95 @@ var renderPictureList = function (photos) {
 
 var pictureListA = renderPictureList(getPictureList());
 pictureList.appendChild(pictureListA);
+
+// ОТКРЫТИЕ ФОРМЫ ЗАГРУЗКИ И РЕДАКТИРОВАНИЯ ФОТО
+
+var uploadForm = document.querySelector('#upload-file');
+var editForm = document.querySelector('.img-upload__overlay');
+var buttonClose = editForm.querySelector('#upload-cancel');
+var photoHashtags = editForm.querySelector('.text__hashtags');
+var photoDescription = editForm.querySelector('.text__description');
+
+  var openEditForm = function () {
+   editForm.classList.remove('hidden');
+   document.addEventListener('keydown', onEditFormEscPress);
+ };
+
+  var closeEditForm = function () {
+   editForm.classList.add('hidden');
+   document.removeEventListener('keydown', onEditFormEscPress);
+ };
+
+  var onInputTagsDescriptionPress = function (evt) {
+   if (evt.keyCode === ESC_BUTTON) {
+     evt.stopPropagation();
+   }
+ };
+
+  var onEditFormEscPress = function (evt) {
+   if (evt.keyCode === ESC_BUTTON) {
+     editForm.classList.add('hidden');
+   }
+ };
+
+ uploadForm.addEventListener('change', openEditForm);
+ buttonClose.addEventListener('click', closeEditForm);
+ photoHashtags.addEventListener('keydown', onInputTagsDescriptionPress);
+ photoDescription.addEventListener('keydown', onInputTagsDescriptionPress);
+
+  // ВАЛИДАЦИЯ ХЕШТЕГОВ
+
+  photoHashtags.addEventListener('invalid', function () {
+   if (photoHashtags.validity.tooShort) {
+     photoHashtags.setCustomValidity('Слишком короткий хештег');
+   }
+ });
+
+  var hashtagHandler = function () {
+   var hashtags = photoHashtags.value;
+
+    if (!hashtags.includes(' ')) {
+     return photoHashtags.setCustomValidity('Хештеги должны разделяться пробелами');
+   }
+
+    hashtags = hashtags.replace(/\s\s+/g, ' ').trim().toLowerCase().split(' ');
+
+    if (hashtags.length > TAG_NUMBER) {
+     return photoHashtags.setCustomValidity('Cлишком много хештегов, ограничьтесь пятью');
+   }
+
+    for (var i = 0; i < hashtags.length; i++) {
+     if (hashtags[i][0] !== '#') {
+       return photoHashtags.setCustomValidity('Хештеги должны начинаться со знака "#"');
+     } else if (hashtags[i].length > 20) {
+       return photoHashtags.setCustomValidity('Длина одного хештега не должна быть больше 20 знаков, включая "#"');
+     } else if (hashtags.findIndex(function (tag, currentIndex) {
+       return tag === hashtags[i] && currentIndex !== i;
+     }) !== -1) {
+       return photoHashtags.setCustomValidity('Хештеги не должны быть одинаковыми');
+     }
+   }
+
+    return hashtags;
+ };
+
+  photoHashtags.addEventListener('change', hashtagHandler);
+
+  photoHashtags.addEventListener('input', function () {
+   photoHashtags.setCustomValidity('');
+ });
+
+  // ИЗМЕНЕНИЕ ГЛУБИНЫ ЭФФЕКТА ФИЛЬТРА
+
+  var EFFECT_LEVEL_LINE_WIDTH = 455; // ширина блока линейки 495px минус отступы линии от границ блока слева справа по 20px
+
+  var effectLevelPin = editForm.querySelector('.effect-level__pin');
+ var effectLevelValue = editForm.querySelector('.effect-level__value');
+ var effectLevelDepth = editForm.querySelector('.effect-level__depth');
+
+  var onEffectLevelPinPress = function () {
+   var currentEffectValue = Math.round(effectLevelDepth / (EFFECT_LEVEL_LINE_WIDTH / 100));
+   effectLevelValue.setAttribute('value', currentEffectValue);
+ };
+
+  effectLevelPin.addEventListener('mouseup', onEffectLevelPinPress);
