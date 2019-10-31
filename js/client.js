5a131bc19f88c44760b2dba2c4eb722f95baa795
Mariazo <zmbereza@gmail.com>
2019-10-31 14:12:40 +0300

    travis

--- js/client.js
+++ js/client.js
@@ -1,37 +1,37 @@
 'use strict';
 
-  (function () {
-   var BASE_URL = 'https://js.dump.academy/kekstagram/';
+(function () {
+  var BASE_URL = 'https://js.dump.academy/kekstagram/';
 
-    var get = function (url, onSuccess, onError) {
-     var xhr = new XMLHttpRequest();
+  var get = function (url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
 
-      xhr.responseType = 'json';
+    xhr.responseType = 'json';
 
-      xhr.addEventListener('load', function () {
-       if (xhr.status === 200) {
-         onSuccess(xhr.response);
-       } else {
-         onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-       }
-     });
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
 
-      xhr.addEventListener('error', function () {
-       onError('Произошла ошибка соединения');
-     });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
 
-      xhr.addEventListener('timeout', function () {
-       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-     });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
 
-      xhr.timeout = 10000; // 10s
+    xhr.timeout = 10000; // 10s
 
-      xhr.open('GET', url);
-     xhr.send();
-   };
+    xhr.open('GET', url);
+    xhr.send();
+  };
 
-    window.client = {
-     BASE_URL: BASE_URL,
-     get: get
-   };
- })();
+  window.client = {
+    BASE_URL: BASE_URL,
+    get: get
+  };
+})();
